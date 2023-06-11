54bdbf8fd5917b171d0b8671b45bc0cba9924c56
alpersarac <alper.sarac42@gmail.com>
2023-06-12 02:56:59 +0300

    Redis item push

---
+++ RedisDistributedCache/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Threading.Tasks;
+
+namespace RedisDistributedCache.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IDistributedCache _distributedCache;
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public IActionResult Index()
+        {
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpiration=DateTime.Now.AddMinutes(1);
+             _distributedCache.SetString("name", "alper", options);
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+            ViewBag.Show =_distributedCache.GetString("name");
+            return View();
+        }
+        public IActionResult Remove()
+        {
+            _distributedCache.RemoveAsync("name");
+            return View();
+        }
+    }
+}
--- RedisDistributedCache/Startup.cs
+++ RedisDistributedCache/Startup.cs
@@ -23,6 +23,9 @@ namespace RedisDistributedCache
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStackExchangeRedisCache(options=> {
+                options.Configuration = "localhost:6379";
+            });
             services.AddControllersWithViews();
         }
 
